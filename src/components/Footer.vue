abb0af3807bcca9b5dadccf873cd8bbf2ecaee75
Paul <7432158+paulransfield@users.noreply.github.com>
2022-02-22 16:41:24 +1300

    Fixed button background style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,12 +2,12 @@
   <footer>
     <p>Copyright &copy; 2021</p>
     <router-link to="/about">
-      <button :style="{ background: color }" class="btn">
+      <button :style="{ background: #669900 }" class="btn">
         About
       </button>
     </router-link> 
     <router-link to="/wiki">
-      <button :style="{ background: color }" class="btn1">
+      <button :style="{ background: #990000 }" class="btn">
         Wiki
       </button>
     </router-link>
@@ -15,13 +15,6 @@
 </template>
 
 <style scoped>
-.btn {
-  color: #669900;
-}
-.btn1 {
-  color: #990000;
-}
-
 footer {
   margin-top: 30px;
   text-align: center;
