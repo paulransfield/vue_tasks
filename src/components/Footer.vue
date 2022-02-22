3e17a91b7fa10fd0cf58a84e447c3540025b1519
Paul <7432158+paulransfield@users.noreply.github.com>
2022-02-22 16:25:41 +1300

    Fix buttons in Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,14 +2,15 @@
   <footer>
     <p>Copyright &copy; 2021</p>
     <router-link to="/about">
-    <button :style="{ background: color }" class="btn">
-    About
-  </button></router-link> 
+      <button :style="{ background: color }" class="btn">
+        About
+      </button>
+    </router-link> 
     <router-link to="/wiki">
-        <router-link to="/about">
-    <button :style="{ background: color }" class="btn">
-    Wiki
-  </button></router-link>
+      <button :style="{ background: color }" class="btn">
+        Wiki
+      </button>
+    </router-link>
   </footer>
 </template>
 
